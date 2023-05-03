f93ecea1a1b3f5693e0ecd62170c603903ff3b80
vm.zhemchugova <vm.zhemchugova@tensor.ru>
2023-05-03 19:06:33 +0300

    Внесены изменения в task2

--- homework/task2.py
+++ homework/task2.py
@@ -6,7 +6,7 @@
 def get_list_info(lst):
     min_elem = min(lst)
     max_elem = max(lst)
-    sum_list = sum(lst)
+    sum_list = round(sum(lst))
     average = round(sum_list / len(lst), 2)
     return min_elem, max_elem, sum_list, average
 
